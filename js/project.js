f0b08d4c0d103dc6483c1747bd101b7146c9524f
Kayra <KayraAlat@gmail.com>
2015-11-14 20:12:48 +0000

    Refactored mobile image injection

--- js/project.js
+++ js/project.js
@@ -1,31 +1,51 @@
 (function() {
 
     function isMobile() {
-
         var mobileBreakpoint = 768;
         return screen.width < mobileBreakpoint ? true : false;
+    }
+
+    function getSection(className) {
+        return document.getElementsByClassName(className)[0];
+    }
+
+    function getLinks(section) {
+        return section.getElementsByTagName("a");
+    }
+
+    function injectImages(links, desktop) {
+
+        desktop = desktop || false;
+
+        var i = !desktop ? 0 : 6;
+
+        var address = desktop ? "resources/images/long_image" : "resources/images/square_image";
+
+        for (i; i < links.length; i++){
+            console.log(i);
+            console.log(links);
+            var img = links[i].getElementsByTagName("img")[0];
+            img.src = address + (i + 1) + ".png";
+        }
 
     }
 
     function loadMobileImages() {
 
-        var mobileLayer = document.getElementsByClassName("mobile-layer")[0];
+        var mobileSection = getSection("mobile-section")
 
-        var links = mobileLayer.getElementsByTagName("a");
+        var links = getLinks(mobileSection);
 
-        for (var i = 0; i < links.length; i++){
-            var img = links[i].getElementsByTagName("img")[0];
-            img.src = "resources/images/square_image" + (i + 1) + ".png";
-        }
+        injectImages(links);
 
-        mobileLayer.style.display = "block";
+        mobileSection.style.display = "block";
 
     }
 
     function loadDesktopImages() {
 
-        var topLayer = document.getElementsByClassName("desktop-top-layer")[0];
-        var bottomLayer = document.getElementsByClassName("desktop-bottom-layer")[0];
+        var topLayer = document.getElementsByClassName("desktop-top-section")[0];
+        var bottomLayer = document.getElementsByClassName("desktop-bottom-section")[0];
 
         var topLinks = topLayer.getElementsByTagName("a");
         var bottomLinks = bottomLayer.getElementsByTagName("a");
