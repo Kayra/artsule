5fc4fb67c495c6e336e9f62e7c37bc08327c70a0
Kayra <KayraAlat@gmail.com>
2015-11-14 12:51:38 +0000

    Added dynamic loading of desktop images

--- js/project.js
+++ js/project.js
@@ -22,8 +22,32 @@
 
     }
 
+    function loadDesktopImages() {
+
+        var topLayer = document.getElementsByClassName("desktop-top-layer")[0];
+        var bottomLayer = document.getElementsByClassName("desktop-bottom-layer")[0];
+
+        var topLinks = topLayer.getElementsByTagName("a");
+        var bottomLinks = bottomLayer.getElementsByTagName("a");
+
+        for (var i = 0; i < topLinks.length; i++){
+            var img = topLinks[i].getElementsByTagName("img")[0];
+            img.src = "resources/images/long_image" + (i + 1) + ".png";
+        }
+
+        for (var i = 0; i < bottomLinks.length; i++){
+            var img = bottomLinks[i].getElementsByTagName("img")[0];
+            img.src = "resources/images/long_image" + (i + 6) + ".png";
+        }
+
+        topLayer.style.display = "block"
+        bottomLayer.style.display = "block"
+    }
+
     if (isMobile()) {
         loadMobileImages();
+    } else {
+        loadDesktopImages();
     }
 
 }());
