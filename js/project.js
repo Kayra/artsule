7d37d2b474818212eebab8918e9b6d9ac9cd7efa
Kayra <KayraAlat@gmail.com>
2015-11-14 12:27:22 +0000

    Added dynamic loading of mobile images

--- js/project.js
+++ js/project.js
@@ -3,14 +3,27 @@
     function isMobile() {
 
         var mobileBreakpoint = 768;
-
         return screen.width < mobileBreakpoint ? true : false;
 
     }
 
-    if (isMobile()) {
-        console.log('hit');
+    function loadMobileImages() {
+
+        var mobileLayer = document.getElementsByClassName("mobile-layer")[0];
+
+        var links = mobileLayer.getElementsByTagName("a");
+
+        for (var i = 0; i < links.length; i++){
+            var img = links[i].getElementsByTagName("img")[0];
+            img.src = "resources/images/square_image" + (i + 1) + ".png";
+        }
+
+        mobileLayer.style.display = "block";
+
     }
-    console.log(screen.width);
+
+    if (isMobile()) {
+        loadMobileImages();
+    }
 
 }());
