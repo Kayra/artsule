4a0b5b5819d73175fc62e88e69625d12533eee4e
Kayra <KayraAlat@gmail.com>
2015-12-05 15:22:29 +0000

    Added image load animation

--- js/project.js
+++ js/project.js
@@ -15,10 +15,25 @@
 
     }
 
+    function loadAnimation() {
+
+        var images = document.querySelectorAll('.desktop-top-section img, .desktop-bottom-section img');
+
+        var i = 0;
+        var interval = setInterval(function() {
+            images[i].classList.add('imageAnimation');
+            ++i;
+            if (i === images.length) {
+                clearInterval(interval);
+            }
+        }, 200);
+    }
+
     if (isMobile()) {
         loadImages('mobile-src');
     } else {
         loadImages('desktop-src');
+        loadAnimation();
     }
 
 }());
