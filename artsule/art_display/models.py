6da485f1e770006566efef2a2edbee97fdaaa39a
Kayra <KayraAlat@gmail.com>
2016-04-28 18:18:35 +0100

    Added initial model

--- artsule/art_display/models.py
+++ artsule/art_display/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class ArtWork(models.Model):
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    imageFile = models.ImageField()
+
+    def __str__(self):
+        return self.name
