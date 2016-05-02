494c82e65b494bcc093c04892c4a2b8995ab7809
Kayra <KayraAlat@gmail.com>
2016-05-02 21:50:26 +0100

    Added dynamic data loading to templates

--- artsule/art_display/models.py
+++ artsule/art_display/models.py
@@ -5,7 +5,9 @@ class ArtWork(models.Model):
 
     name = models.CharField(max_length=255)
     description = models.TextField()
-    imageFile = models.ImageField()
+    largeImage = models.ImageField()
+    desktopImage = models.ImageField()
+    mobileImage = models.ImageField()
 
     def __str__(self):
         return self.name
